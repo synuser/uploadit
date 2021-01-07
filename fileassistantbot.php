f700aa649a2afaa82e3c96788b4595df06d1d9c2
Syn <38806740+synuser@users.noreply.github.com>
2021-01-07 10:18:30 +0000

    Update fileassistantbot.php

--- fileassistantbot.php
+++ fileassistantbot.php
@@ -10,7 +10,7 @@
  * https://t.me/TheDarkW3b
  */
 define('FILES_PATH', __DIR__.'/files');
-define('WEBSERVER_BASE_URL', 'https://synfiles.herokuapp.com/');
+define('WEBSERVER_BASE_URL', 'https://uploadfiles.io');
 define('FILES_EXPIRE_TIME', 24 * 3600); // in seconds
 
 set_time_limit(0);
